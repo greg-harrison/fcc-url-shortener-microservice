3c68efb7245020723a83eac6a6a7c8659ac687d0
Greg <greg@Gregs-MacBook-Pro.local>
2017-09-05 09:15:16 -0400

    Further into the setup and some planning

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const express = require('express')
 const pug = require('pug')
 const middleware = require('./middleware')
+// Need to add a Controllers file for the validations and for the Mongoose setup
+//require mongoose
 
 const compiledTemplate = pug.compileFile('templates/template.pug')
 const app = express()
 
 app.use(middleware.malformedUrl)
 
-app.get('/', (req, res) => {
-  res.send(compiledTemplate())
+app.get('/:outputUrl?', (req, res) => {
+  let payload = {}
+
+  if (req.params.outputUrl) {
+    // Check to see if the url is valid
+
+    let urlValid
+
+    if (urlValid) {
+
+    } else {
+      payload.error = 'Invalid Url'
+      res.send(compiledTemplate({ payload }))
+    }
+  } else {
+    res.send(compiledTemplate({ payload }))
+  }
+})
+app.get('/create/:inputUrl', (req, res) => {
+  console.log('req', req);
 })
 
-app.listen(8000)
+app.listen(8000, () => {
+  console.log('The server is running on port 8000');
+})
